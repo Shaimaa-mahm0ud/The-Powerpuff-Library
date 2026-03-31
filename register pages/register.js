5586ae232d5dd6520175136ee94c5eeef463a92d
alaa <lolomamdo2006@gmail.com>
2026-03-31 21:03:51 +0200

    yaaaarb

--- register pages/register.js
+++ register pages/register.js
@@ -1,32 +1,43 @@
 
 const admins=[];
     admins[0] = {
-      username: "alaa",
-      email: "alaa@gmail.com",
+      username: "admin",
+      email: "admin@gmail.com",
+      password: "123"
+    };
+const users=[];
+    users[0] = {
+      username: "user",
+      email: "user@gmail.com",
       password: "123"
     };
 const form = document.getElementById("login");
 let heIsAdmin=false;
+let heIsUser=false;
 
 form.addEventListener("submit", function(e){
   e.preventDefault(); 
 
   
   const username = document.getElementById("username").value;
-//   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   
   for(i in admins){
     if( admins[i].username==username&&admins[i].password==password){
        heIsAdmin=true;
-       localStorage.setItem("loggedIn", "true");   
-        localStorage.setItem("username", username);
-         window.location.href = "../welcome page/welcome.html";
+         window.location.href = "../Book details/Admin Books list.html";
        break;
     }
   }
-    if (!heIsAdmin) {
-    
+  for(i in users){
+    if( users[i].username==username&&users[i].password==password){
+       heIsUser=true;
+
+         window.location.href = "../Book details/User Books list.html";
+       break;
+    }
+  }
+    if (!heIsAdmin &&!heIsUser) {
     alert("Username or password is incorrect!");
   }
 
